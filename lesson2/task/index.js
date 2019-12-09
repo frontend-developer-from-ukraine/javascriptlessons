bf9f0c4980a616e763adb5e3e4fd992e43177d2a
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-09 14:31:18 +0200

    task

---
+++ lesson2/task/index.js
@@ -0,0 +1,12 @@
+/*
+
+const userName = 'John';
+const greeting = "I'm John";
+const profession = 'Developer';
+const $hobby = 'Walking';
+const avouriteDESSERT = 'cake';
+let pet_ = 'dog';
+let superNumber;
+superNumber = 17;
+
+*/

5f82f483dc7fb8f4b75957ba13d4485789f8f0d2
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-10 18:42:37 +0200

    task1

---
+++ lesson4/task5/index.js
@@ -0,0 +1,19 @@
+let m = 10;
+let n = 5;
+
+let result = 0;
+
+for (let a = m; a <= n; a++) {
+
+    if (a % 2 === 0 && a % 4 !== 0) {
+        result += a;
+    } else if (i % 3 === 0) {
+        result -= a;
+    } else if (a % 4 === 0) {
+        result *= a;
+    }
+
+    if (a % 5 === 0) {
+        console.log(a);
+    }
+}

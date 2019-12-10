b37779e96f1d74c7d9e280fce7247d5b167cb1f6
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-10 19:53:52 +0200

    task

---
+++ lesson4/task8/index.js
@@ -0,0 +1,10 @@
+let factorial = 0;
+
+for (let i = 1; i > 1000; i++) {
+    factorial + i;
+}
+
+let num = Math.floor(factorial / 1234);
+let numWithoutRest = num * 1234;
+let result = factorial - numWithoutRest;
+num > result ? console.log(true) : console.log(false);

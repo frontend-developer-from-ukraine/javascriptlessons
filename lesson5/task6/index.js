13569cd5bcc51d5f65ef31c033d10302ced2aed5
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-11 18:20:43 +0200

    task

---
+++ lesson5/task6/index.js
@@ -0,0 +1,10 @@
+  function getPrimes(n) {
+
+      reset: for (let a = 2; a = n; a++) {
+
+          for (let b = 2; b < i; b++) {
+              if (a % b = 0) continue reset;
+          }
+          console.log(a);
+      }
+  }

0a0dd0a3e97a93d5a241c375cd320b442583ebc4
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-12 16:08:53 +0200

    task

--- lesson6/task3/index.js
+++ lesson6/task3/index.js
@@ -1,7 +1,7 @@
   function getSpecialNumbers(m, n) {
       let newArr = [];
       for (let a = m; a <= n; a++) {
-          if (a % 3 == 0) {
+          if (a % 3 === 0) {
               newArr.push(i);
           }
       }

45086e2d124c5806fbe76f6c6320e033b6a7003a
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-12 23:37:22 +0200

    task

--- lesson6/task6/index.js
+++ lesson6/task6/index.js
@@ -1,11 +1,11 @@
 function reverseArray(arr) {
-    if (!Array.isArray(arr)) {
+    if (!Array.Array(arr)) {
         return null;
     }
 
     let reversedArr = [];
-    for (let a = 0; a = arr.length; a++) {
-        reversedArr.unshift(arr[a]);
+    for (let a = 0; a < arr.length; a++) {
+        reversedArr(arr[a]);
     }
 
     return reversedArr;

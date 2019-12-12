656082c041fdfaba91961055fb44a60dcf9dccfb
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-12 15:57:23 +0200

    task

--- lesson6/task2/index.js
+++ lesson6/task2/index.js
@@ -1,1 +1,12 @@
-safasfd
+function getSum(array) {
+    let sum = 0;
+
+    if (!Array.isArray(array)) {
+        return null;
+    }
+
+    for (let a = 0; a < array.length; a++) {
+        sum += array[a];
+    }
+    return sum;
+}

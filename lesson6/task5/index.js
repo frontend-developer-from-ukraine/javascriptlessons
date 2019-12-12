fefa49eb77e44e73d6519e8398dc88f49f7b4e02
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-12 20:04:20 +0200

    task

--- lesson6/task5/index.js
+++ lesson6/task5/index.js
@@ -1,13 +1,8 @@
 function squareArray(arr) {
     let squaredArr = [];
-    ull;
-}
 
-for (let a = 0; a < arr.length; a++) {
-    squaredArr.push(arr[a] * arr[a]);
-}
 
-return squaredArr;
+    return squaredArr;
 }
 
 console.log(squareArray([2, 3, 4, 5]));

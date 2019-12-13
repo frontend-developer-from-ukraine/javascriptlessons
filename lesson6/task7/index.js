9a1ce3c5852bd7cae83a079fc9dd5a4b1f624ae5
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-13 13:32:41 +0200

    task

---
+++ lesson6/task7/index.js
@@ -0,0 +1,15 @@
+function sortAsc(array) {
+    for (let a = 0; a < array.length; a++) {
+        for (let b = 0; b < array.length; b++) {
+
+        }
+    }
+    return array;
+}
+
+function sortDesc(array) {
+    for (let a = 0; a < array.length; a++) {
+        for (let b = 0; b < array.length; b++)
+    }
+    return array;
+}

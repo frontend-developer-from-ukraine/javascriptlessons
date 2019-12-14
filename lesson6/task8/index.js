f84ef2575a0bc6f3fc61a90c42d965c937714ed6
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-14 13:57:33 +0200

    task

--- lesson6/task8/index.js
+++ lesson6/task8/index.js
@@ -1,5 +1,5 @@
-  function uniqueCount(array) {
-      let long = 0;
-      let q = array.filter((item, index) > index == array.indexOf(item) ? long++ : false);
-      return long;
-  }
+function uniqueCount(array) {
+    let long = 0;
+    let a = array.filter((item, index) => index == array.indexOf(item) ? long++ : false);
+    return long;
+}

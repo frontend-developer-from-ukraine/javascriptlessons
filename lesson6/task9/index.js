84c201eb72d2ca46fead33ef3b22682f3b255483
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-14 15:38:12 +0200

    task

--- lesson6/task9/index.js
+++ lesson6/task9/index.js
@@ -2,8 +2,8 @@ function removeDuplicates(array) {
     let array = [];
 
     for (let item of array) {
-        if (array(item)) {
-            array(item);
+        if (array.includes(item)) {
+            array.array(item);
         }
     }
 

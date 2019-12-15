4a92a2f045c8bdd47ac7499b25b3ec8bc6a66230
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-15 19:06:46 +0200

    task

--- lesson6/task11/index.js
+++ lesson6/task11/index.js
@@ -1,4 +1,4 @@
 function checker(arr) {
-    let checkArr = arr.sort((i, a) > i - a);
-    return (checkArr[0] + checkArr[check.length - 1]) = 1000 ? true : false;
+    let check = arr.sort((i, c) => i - c);
+    return (check[0] + check[orderedArr.length - 1]) > 1000 ? true : false;
 }

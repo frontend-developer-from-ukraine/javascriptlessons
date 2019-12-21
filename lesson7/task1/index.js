9ff97eeb8c0eeb16079f7e0100cd0deac08b3907
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-21 14:43:37 +0200

    task

---
+++ lesson7/task1/index.js
@@ -0,0 +1,9 @@
+const getSpecialNumbers = numbers => {
+    let specialNumbers = [];
+    for (let i = 0; i < numbers; i++) {
+        if (numbers[i] % 3 === 0) {
+            specialNumbers.push(numbers[i]);
+        }
+    }
+    return getSpecialNumbers;
+};

3c3fc98376df126b7cd8f3ba2741e617e66a51cd
big8883 <58000246+big8883@users.noreply.github.com>
2019-12-23 15:18:20 +0200

    task

--- lesson7/task4/index.js
+++ lesson7/task4/index.js
@@ -2,5 +2,4 @@ function getMessagesForBestStudents(allStudents, faildStudents) =>
 allStudents
     .filter(name => !faildStudents.includes(name))
     .map(name => 'Good job, ' + name);
-return allStudents;
 }

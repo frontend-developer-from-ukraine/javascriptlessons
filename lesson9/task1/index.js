73eb1aa1c93a8ea246a014929bfbd3431cdf2c08
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-01-30 09:25:26 -0800

    add

--- lesson9/task1/index.js
+++ lesson9/task1/index.js
@@ -1,4 +1,4 @@
-const getAduls = usersObj =>
-    Object.entries(usersObj)
-    .filter(user => user[1] >= 18)
-    .map(user => user[0]);
+const getAdults = obj =>
+    Object.entries(obj)
+    .filter(person => person[1] >= 18)
+    .map(person => person[0]);

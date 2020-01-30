a2d9abacae656af5693043b986f56328112d06d8
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-01-30 06:21:30 -0800

    add

--- lesson8/task3/index.js
+++ lesson8/task3/index.js
@@ -1,12 +1,16 @@
 const pickProps = (obj, arr) => {
-    let obj = {};
+    let resultObj = {};
 
     for (let i of arr) {
         for (let key in obj) {
-            if (i === key) {
-                obj[key] = obj[key];
+            if (i == key) {
+                resultObj[key] = obj[key];
             }
         }
     }
-    return obj;
+    return resultObj;
 }
+
+const arr = ['a', 17, 'Max'];
+const result = pickProps(arr);
+console.log(result);

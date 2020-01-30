b7f047e588151918cdb2ab16176406bd1103274a
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-01-30 02:57:49 -0800

    add

--- lesson8/task3/index.js
+++ lesson8/task3/index.js
@@ -1,3 +1,12 @@
-function {
+const pickProps = (obj, arr) => {
+    let obj = {};
 
+
+    for (let key in obj) {
+        if (i == key) {
+            obj[key] = obj[key];
+        }
+    }
+
+    return obj;
 }

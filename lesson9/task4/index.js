78537ebd4957a36282c0338198ec9df7a4da333d
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-01-31 05:37:30 -0800

    add

--- lesson9/task4/index.js
+++ lesson9/task4/index.js
@@ -1,3 +1,3 @@
 const getPeople = obj => Object.values(obj)
-    .reduce((acc, elem) => acc.concat(), [])
-    .map(elem => elem);
+    .reduce((acc, elem) => acc.concat(elem), [])
+    .map(elem => elem.name);

83d3de9f70f5e163c5b8370a36282653b73de0da
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-03 06:59:47 -0800

    add

--- lesson10/task5/index.js
+++ lesson10/task5/index.js
@@ -1,5 +1,6 @@
 const getTotalPrice = arr => {
-    let sum = arr.map((acc, el) => acc + el);
+    let sum = arr.reduce((acc, el) => acc + el);
     let floor = Math.floor(sum * 100) / 100;
     return '$' + floor;
+
 };

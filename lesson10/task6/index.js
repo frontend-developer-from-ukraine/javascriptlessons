84c4ef93ce58fd8226bb108c43c3024c3d3a78f6
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-03 08:26:00 -0800

    add

--- lesson10/task6/index.js
+++ lesson10/task6/index.js
@@ -2,6 +2,7 @@ const getRandomNumbers = (length, start, end) => {
     const numbers = (end - start < 1) && Math.ceil(end) === Math.ceil(start);
     if (start > end || numbers) return null;
 
+
     return Array(length)
-        .map(i => Math.random) * (end - start) + start);
+        .map(i => Math.random * (end - start) + start);
 }

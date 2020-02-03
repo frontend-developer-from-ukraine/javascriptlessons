1bd7a2d4b0bfa0031adc8be2e4f5034eac3bd4a4
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-03 07:13:30 -0800

    add

--- lesson10/task5/index.js
+++ lesson10/task5/index.js
@@ -1,6 +1,12 @@
 const getTotalPrice = arr => {
-    let sum = arr.reduce(el => el + el);
+    let sum = arr.reduce((acc, el) => acc + el);
     let round = Math.floor(sum * 100) / 100;
-    return sum + round;
-
+    return "$" + round;
 };
+const prices = [56.23, 566.45345, 4.3435]; // $627.02
+console.log(getTotalPrice(prices));
+
+//сумма дробных чисел (Алгоритм)
+//1. Создать переменную - сложить ценны через метод массивов 
+//2. Создать переменную - округлить 
+//3. вернуть сумму с округлением

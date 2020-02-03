0d33c1225818001b6ece70ce3863a01a6194c13b
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-03 05:27:28 -0800

    add

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,10 +1,13 @@
 const getMaxAbsolutNumber = arr => {
-    let max = -Infinity;
 
-    arr.forEach(num => {
-        if (Math.abs(num) > max) {
-            max = Math.abs(num);
-        }
-    });
-    return max;
+    if (!Array.isArray(arr) || arr.length < 1) {
+        return null;
+    }
+
+
+    const absoluteValues = arr
+        .map(num => Math.abs(num));
+
+    return Math.max(...absoluteValues);
+
 }

c0461598ad1c3cb62a5733ba76f1d36011fa1e9c
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-03 09:18:13 -0800

    add

--- lesson10/task7/index.js
+++ lesson10/task7/index.js
@@ -1,10 +1,13 @@
 const superRound = (number, rounding) => {
+
     const calculation = Math.pow(10, rounding);
-    [
+
+    return [
         Math.floor(number * calculation) / calculation,
         Math.round(number * calculation) / calculation,
         Math.ceil(number * calculation) / calculation,
         Math.trunc(number * calculation) / calculation,
         Number(number.toFixed(rounding))
     ];
+
 }

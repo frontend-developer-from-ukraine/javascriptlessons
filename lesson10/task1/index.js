03ecfb8946c9781ea01a42e3e63c7ec71c22db81
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-03 02:49:53 -0800

    correction

--- lesson10/task1/index.js
+++ lesson10/task1/index.js
@@ -1,14 +1,24 @@
-const getFiniteNumbers = arr =>
+const getFiniteNumbers = arr => // выводит только числа 
     arr.filter(el => Number.isFinite(el));
 
-const getFiniteNumbersV2 = arr =>
+const getFiniteNumbersV2 = arr => // вывело и числа и строки
     arr.filter(el => isFinite(el));
 
-const getNaN = arr =>
+const getNaN = arr => // вывело пустой массив
     arr.filter(el => Number.isNaN(el));
 
-const getNaNV2 = arr =>
+const getNaNV2 = arr => // только текст
     arr.filter(el => isNaN(el));
 
-const getIntegers = arr =>
+const getIntegers = arr => // вывело только целое число 
     arr.filter(el => Number.isInteger(el));
+
+
+
+const elementList = [5, 6.6, 4.3, '55', 'text'];
+console.log(getIntegers(elementList));
+
+// Проверка чисел(алгоритм):
+// 1.	создать функцию 
+// 2.	которая принимает массив 
+// 3.	Отфильтровать + Поиск чисел

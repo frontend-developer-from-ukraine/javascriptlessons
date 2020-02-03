1eabba9aea233d329da2ea912b1792f73f79e42a
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-03 02:42:37 -0800

    add

---
+++ lesson10/task1/index.js
@@ -0,0 +1,14 @@
+const getFiniteNumbers = arr =>
+    arr.filter(el => Number.isFinite(el));
+
+const getFiniteNumbersV2 = arr =>
+    arr.filter(el => isFinite(el));
+
+const getNaN = arr =>
+    arr.filter(el => Number.isNaN(el));
+
+const getNaNV2 = arr =>
+    arr.filter(el => isNaN(el));
+
+const getIntegers = arr =>
+    arr.filter(el => Number.isInteger(el));

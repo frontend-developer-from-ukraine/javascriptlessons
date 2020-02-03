f8f59969a2fdbe753cb74c6ddd00e70b2a542537
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-03 03:15:17 -0800

    add

---
+++ lesson10/task2/index.js
@@ -0,0 +1,11 @@
+const getParsedIntegers = arr =>
+    arr.map(el => Number.parseInt(el));
+
+const getParsedIntegersV2 = arr =>
+    arr.map(el => parseInt(el));
+
+const getParsedFloats = arr =>
+    arr.map(el => Number.parseFloat(el));
+
+const getParsedFloatsV2 = arr =>
+    arr.map(el => parseFloat(el));

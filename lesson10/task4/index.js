5f04dc5a83f6a62bc53d1b4cc5ee36d262f1dcca
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-03 05:53:40 -0800

    add

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -2,12 +2,10 @@ function getMaxAbsoluteNumber = arr => {
     if (!Array.isArray(arr) || arr.length < 1) {
         return null;
     }
-
     const absoluteValues = arr.map(num => {
-        return Math.abs(num)
+        return Math.abs(num);
     });
-
     return Math.max(...absoluteValues);
-
-
 };
+
+console.log(getMaxAbsoluteNumber([34, 55.3, 23.4, 99.33]));

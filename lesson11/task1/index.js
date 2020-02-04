e70e77f3ec0bc8ab7e044038fc89c5d4512539bb
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-04 03:01:13 -0800

    add

--- lesson11/task1/index.js
+++ lesson11/task1/index.js
@@ -7,7 +7,7 @@ const splitText = (text, len) => {
         if (chunk.length === 0) {
             break;
         }
-        strArr.push(chunk[0].toUpperCase() + chunk.slice[1]);
+        strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
         startPosition += len;
     }
     return strArr.join('\n');

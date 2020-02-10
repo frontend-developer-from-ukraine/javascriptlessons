9638caf7ce4455549242bdc5fdd5e91c8446b1e9
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-10 04:50:11 -0800

    add with theory

--- lesson12/hw1/index.js
+++ lesson12/hw1/index.js
@@ -1,6 +1,25 @@
 const reverseString = (str) => {
-    if (typeof str !== "string") {
-        return null;
+        if (typeof str !== "string") { // если передана не строка возвращаем null
+            return null;
+        }
+        return str.split('').reverse().join('');
     }
-    return str.split('').reverse().join('');
-}
+    // 
+'some text'.split('')['some', 'text']
+    /*
+    const reversed = array1.reverse();
+    console.log('reversed:', reversed);
+    // expected output: "reversed:" Array ["three", "two", "one"]
+
+    /*
+    const elements = ['Fire', 'Air', 'Water'];
+
+    console.log(elements.join());
+    // expected output: "Fire,Air,Water"
+
+    console.log(elements.join(''));
+    // expected output: "FireAirWater"
+
+    console.log(elements.join('-'));
+    // expected output: "Fire-Air-Water"
+    */

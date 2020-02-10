c21cfc10524664dca3702f634d4e2f4527d62599
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-10 04:53:16 -0800

    with theory and algorithm

--- lesson12/hw1/index.js
+++ lesson12/hw1/index.js
@@ -2,7 +2,7 @@ const reverseString = (str) => {
         if (typeof str !== "string") { // если передана не строка возвращаем null
             return null;
         }
-        return str.split('').reverse().join('');
+        return str.split('').reverse().join(''); // 1. строку на строки 2. перевернули в обратную строку 3. сложили 
     }
     // 
 'some text'.split('')['some', 'text']

320679fd32b561d91f8527283f256137f17939ed
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-10 02:37:59 -0800

    add

--- lesson11/hw1/index.js
+++ lesson11/hw1/index.js
@@ -1,26 +1,26 @@
 const splitString = (text, number) => {
 
-    if (typeof text !== 'string') {
+    if (typeof text !== 'string') { // если не строка вернуть null
         return null;
     }
 
-    const strArr = [];
-    let startPosition = 0;
+    const strArr = []; // все строки записываем в массив
+    let startPosition = 0; // переменная отсчета
 
-    while (true) {
-        let chunk = text.substr(startPosition, number);
-        if (chunk.length === 0) {
-            break;
+    while (true) { // используем бесконечный цикл , который будет идти по строке и отрезать определенный кусок
+        let chunk = text.substr(startPosition, number); // на каждой итерации отрезаем кусок , метод .substr() отрезает кусок
+        if (chunk.length === 0) { // если длина ноль то заканчиваем считывать строку
+            break; // директивой обрываем цикл
         }
 
-        startPosition += number;
-        strArr.push(chunk);
+        startPosition += number; // складываем 
+        strArr.push(chunk); // пушаем в массив 
     }
 
-    let lastElement = strArr[strArr.length - 1];
+    let lastElement = strArr[strArr.length - 1]; // берем последний элимент массива
 
-    lastElement += '.'.repeat(number - lastElement.length);
-    strArr[strArr.length - 1] = lastElement;
+    lastElement += '.'.repeat(number - lastElement.length); // последний элемент складываем 
+    strArr[strArr.length - 1] = lastElement; // выводим посл элм
 
-    return strArr;
+    return strArr; // вернуть массив
 }

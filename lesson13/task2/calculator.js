4ccf65d2993e895fb604abd41bb2613950307f96
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-11 01:50:24 -0800

    add with teory

--- lesson13/task2/calculator.js
+++ lesson13/task2/calculator.js
@@ -1,8 +1,8 @@
-export const getSquaredArray = arr =>
-    arr.map(num => num * num);
+export const getSquaredArray = arr => // перед функцией указываем export - что дает добро на экспорт для теста
+    arr.map(num => num * num); // пройтись по массиву и возвести в квадрат все элтиенты , квадрат это число умножить на самого себя
 
 
 export const getOddNumbers = numbers =>
-    numbers.filter(num => (num % 2 === 1));
+    numbers.filter(num => (num % 2 === 1)); // отфильтровать , оставить только не четные 
 
-export default (a, b) => a + b;
+export default (a, b) => a + b; // return и ковычки сократили , сложили два числа и вернули их
--- lesson13/task2/calculator.test.js
+++ lesson13/task2/calculator.test.js
@@ -1,15 +1,15 @@
-import getSum, { getSquaredArray, getOddNumbers } from './calculator'
+import getSum, { getSquaredArray, getOddNumbers } from './calculator' // импорт функций с папки 
 
 
-it('should get squared numbers', () => {
+it('should get squared numbers', () => { // что делает делает проверка 
     const result = getSquaredArray([1, 2, 3]);
-    expect(result).toEqual([1, 4, 9]);
+    expect(result).toEqual([1, 4, 9]); // ожидаем(), равно([])
 });
-it('should keep odd numbers', () => {
+it('should keep odd numbers', () => { // что делает делает проверка 
     const result = getOddNumbers([1, 2, 3, 4, 5]);
-    expect(result).toEqual([1, 3, 5]);
+    expect(result).toEqual([1, 3, 5]); // ожидаем(), равно([])
 });
-it('should get sum of numbers', () => {
+it('should get sum of numbers', () => { // что делает делает проверка 
     const result = getSum(8, 4);
-    expect(result).toEqual(12);
+    expect(result).toEqual(12); // ожидаем(), равно([])
 });

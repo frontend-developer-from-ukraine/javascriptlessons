b116f47c68dcd4e01379eba1e482ad16f3833959
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-13 06:38:34 -0800

    add

--- lesson15/hw2/index.js
+++ lesson15/hw2/index.js
@@ -2,22 +2,22 @@ export function createLogger() {
     let memory = [];
 
     function warn(warn) {
-        return memory.concat({ text: 'warn' });
+        return memory.concat({ message: warn, dateTime: new Date(), type: 'warn' });
     }
 
     function error(error) {
-        return memory.concat({ text: 'error' });
+        return memory.concat({ message: error, dateTime: new Date(), type: 'error' });
     }
 
     function log(log) {
-        return memory.concat({ text: 'log' })
+        return memory.concat({ message: log, dateTime: new Date(), type: 'log' })
     }
 
     function getRecords(str) {
         if (!str) {
             return memory
         };
-        return memory.filter(item => item.text === str);
+        return memory.filter(item => item.type === string).sort((a, b) => a.dateTime - b.dateTime);
     }
     return {
         warn,

793574bdb6bf69bd6253c3042bd2fbd13c0bb718
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-17 13:01:12 -0800

    add

--- lesson17/hw3/index.js
+++ lesson17/hw3/index.js
@@ -1,7 +1,5 @@
-export const bind = function(func, context) {
-    let bindArgs = slice.call(arguments, 2);
-    return function() {
-        let funcArgs = slice(arguments);
-        return func.apply(context, bindArgs.concat(funcArgs));
+export function bind(func, context, ...arg) {
+    return function(...finalnArg) {
+        return func(context, ...arg, ...finalArg);
     };
-};
+}

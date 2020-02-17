3033fd0c858782f5528407c66f2c82f864c89394
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-17 10:26:01 -0800

    add

--- lesson17/task3/index.js
+++ lesson17/task3/index.js
@@ -3,3 +3,9 @@ export function defer(func, ms) {
         setTimeout(() => func.call(this, ...arguments), ms);
     }
 };
+
+export function defer(func, ms) {
+    return function() {
+        setTimeout(() => func.apply(this, arguments), ms);
+    }
+};

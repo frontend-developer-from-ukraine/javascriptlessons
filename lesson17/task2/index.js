7bf98e1dbabec457d46f9f2d66cf42276b067fbd
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-17 09:45:49 -0800

    add

--- lesson17/task2/index.js
+++ lesson17/task2/index.js
@@ -5,7 +5,7 @@ export const callbackPrompt = {
         console.log(phoneNumber);
     },
     showDeferredPrompt(ms) {
-        setTimeout(this.showPrompt, ms)
+        setTimeout(this.showPrompt.bind(this), ms)
     }
 };
 callbackPrompt.showDeferredPrompt(1000);

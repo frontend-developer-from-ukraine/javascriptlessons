1e82c33903d087b906e5fe09dfb6b8a2512d18a0
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-17 12:47:12 -0800

    add

--- lesson17/hw2/index.js
+++ lesson17/hw2/index.js
@@ -1,20 +1,25 @@
 export const timer = {
     secondsPassed: 0,
     minsPassed: 0,
-    intervalId: 0,
-    getTime() {
-        return `${this.minsPassed}:${this.secondsPassed < 10 ? '0' + this.secondsPassed : this.secondsPassed}`;
-    },
     startTimer() {
         this.intervalId = setInterval(() => {
             this.secondsPassed += 5
+            if (this.secondsPassed === 60) {
+                this.minsPassed += 1;
+                this.secondsPassed = 0;
+            }
         }, 5000);
     },
-    stopTimer() {
-        clearInterval(this.intervalId);
+    getTime() {
+        return `${this.minsPassed}:${this.secondsPassed < 10 ? '0' + this.secondsPassed : this.secondsPassed}`;
     },
+
     resetTimer() {
         this.minsPassed = 0;
         this.secondsPassed = 0;
     },
+    stopTimer() {
+        clearInterval(this.intervalId);
+    },
+
 }

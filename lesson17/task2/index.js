c8bc3d5b4ff40f97233e95b5f618bc7b293a8307
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-17 09:44:26 -0800

    add

---
+++ lesson17/task2/index.js
@@ -0,0 +1,11 @@
+export const callbackPrompt = {
+    message: 'Tell me your number',
+    showPrompt() {
+        const phoneNumber = prompt(this.message);
+        console.log(phoneNumber);
+    },
+    showDeferredPrompt(ms) {
+        setTimeout(this.showPrompt, ms)
+    }
+};
+callbackPrompt.showDeferredPrompt(1000);

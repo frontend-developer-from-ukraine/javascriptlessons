5c5310cd9ecba38757e97d024bb31990ea9389d5
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-18 10:04:32 -0800

    add

---
+++ lesson18/task1/index.js
@@ -0,0 +1,17 @@
+export const event = {
+    guests: [
+        { name: 'John', age: 18, email: 'm@gmail.com' },
+        { name: 'Max', age: 17, email: 'max@gmail.com' },
+        { name: 'Ivan', age: 29, email: 'i@gmail.com' }
+    ],
+    message: 'Welcome to the party!',
+    getInvitations() {
+        return this.guests
+            .filter(({ age }) => age >= 18)
+            .map(({ name, email }) => ({
+                text: `Dear ${name}! ${this.message}`,
+                email,
+            }));
+    }
+};
+console.log(event.getInvitations());

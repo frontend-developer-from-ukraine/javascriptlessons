ea770aa42e9f63ccd3d20af658524bc029a8c58f
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-18 12:10:20 -0800

    add

--- lesson18/task3/index.js
+++ lesson18/task3/index.js
@@ -1,6 +1,6 @@
 export function sumOfSquares() {
     return [...arguments].reduce((acc, elem) => {
-        return acc + elem;
+        return acc + elem * elem;
     }, 0);
 }
 console.log(sumOfSquares(4, 3, 4, 1));

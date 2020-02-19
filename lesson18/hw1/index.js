2b9ee440dda122fd86419af36c71dadb9cd4b4aa
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-19 02:08:47 -0800

    add

--- lesson18/hw1/index.js
+++ lesson18/hw1/index.js
@@ -1,9 +1,9 @@
 export function spy(func) {
     saveCalls.calls = [];
 
-    function saveCalls(...args) {
-        saveCalls.calls.push(args)
-        return func.apply(this, args)
+    function saveCalls([args]) {
+        saveCalls.calls.push(...args)
+        return func.apply(this, ...args)
     }
     return saveCalls;
 }

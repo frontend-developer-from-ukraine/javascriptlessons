573e94d2ef4675caa8c24edc769093f6c27c1237
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-20 05:50:37 -0800

    add

--- lesson19/task4/index.js
+++ lesson19/task4/index.js
@@ -9,9 +9,12 @@ User.prototype.requestNewPhoto = function() {
     console.log(`New photo request was sent for ${this.name}`);
 };
 User.prototype.setAge = function(age) {
+    if (age < 0) {
+        return false;
+    }
+    this.age = age;
     if (age >= 25) {
         this.requestNewPhoto
     }
-    return age;
 };
 const user = new User('John', 26);

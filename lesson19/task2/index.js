99b8c4443be9be43c623e51e4e8ebb63395ceba0
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-20 05:31:38 -0800

    add

--- lesson19/task2/index.js
+++ lesson19/task2/index.js
@@ -2,7 +2,7 @@ let obj = {};
 export function getOwnProps(obj) {
     let propArr = [];
     for (let prop in obj) {
-        if (typeof obj[prop] !== 'function')) {
+        if (typeof obj[prop] === 'function')) {
         propArr.push(prop);
     }
 }

d5d575a448e29f6d8cba2e166669f20d266a1ad6
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-23 13:20:21 -0800

    add

---
+++ lesson20/task2/index.js
@@ -0,0 +1,27 @@
+class Vehicle {
+    constructor(name, numberOfWheels) {
+        this.name = name;
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    move() {
+        console.log(`${this.name} is moving`);
+    }
+    stop() {
+        console.log(`${this.name} stopped`);
+    }
+}
+eclass Ship extends Vehicle {
+    constructor(name, maxSpeed) {
+        super(name, false);
+        this.maxSpeed = maxSpeed;
+    }
+    move() {
+        super.move();
+        console.log(`${this.name} lifting anchor up`);
+    }
+    stop() {
+        super.stop();
+        console.log(`${this.name} lifting anchor down`);
+    }
+}

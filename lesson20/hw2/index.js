00a92e02822d9645058b855161241d455cffe7bd
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-24 10:29:29 -0800

    add

--- lesson20/hw2/index.js
+++ lesson20/hw2/index.js
@@ -1,34 +1,43 @@
 export class User {
     constructor(id, name, sessionId) {
         this._id = id;
-        this._name = name;
-        this._sessionId = sessionId;
     }
     get id() {
         return this._id;
     }
-
-    export class UserRepository {
-        constructor(users) {
-            this._users = Object.freeze(users);
-        }
-        get users() {
-            return this._users;
-        }
-
-        getUserNames() {
-            return this._users.map((a) => a.name);
-        }
-        getUserIds() {
-            return this._users.map((a) => a.id);
-        }
-
+    get name() {
+        return this._name;
     }
-    const newUser = new User('333', 'Max', '1234');
-    const newUser1 = new User('444', 'Tom', '5678');
-    const newUser2 = new User('555', 'Bob', '0987');
-    console.log(newUser);
-    console.log(newUser1);
-    const users1 = new UserRepository([{ id: 123, name: 'Max' }, { id: 456, name: 'Tom' }, { id: 789, name: 'Bob' }])
-    console.log(users1);
-    console.log(users2);
+    get sessionId() {
+        return this._sessionId;
+    }
+}
+
+export class UserRepository {
+    constructor(users) {
+        this._users = Object.freeze(users);
+    }
+    get users() {
+        return this._users;
+    }
+
+    getUserNames() {
+        return this._users.map((a) => a.name);
+    }
+    getUserIds() {
+        return this._users.map((a) => a.id);
+    }
+    getUserNameById(id) {
+        for (let i of this._users) {
+            if (i.id === id)
+                return i.name;
+        }
+    }
+
+}
+const newUser = new User('333', 'Max', '1234');
+const newUser1 = new User('444', 'Tom', '5678');
+console.log(newUser);
+console.log(newUser1);
+const users1 = new UserRepository([{ id: 123, name: 'Max' }, { id: 456, name: 'Tom' }, { id: 789, name: 'Bob' }])
+console.log(users1);

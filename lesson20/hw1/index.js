d49083f631f4f89465c05227a9121e4c582fed51
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-24 09:10:33 -0800

    add

--- lesson20/hw1/index.js
+++ lesson20/hw1/index.js
@@ -2,17 +2,16 @@ export class Order {
     constructor(price, city, type) {
         this.id = (Math.random()).toString();
         this.price = price;
-        this.dateCreated = new Date();
-        this.dateConfirmed = 0;
-        this.isConfirmed = false;
-        this.city = city;
-        this.type = type;
     }
 
     checkPrice() {
         return this.price > 1000;
     }
     confirmOrder() {
+        if (!this.isConfirmed) {
+            this.isConfirmed = true;
+        }
+        this.dateConfirmed = new Date();
 
     }
     isValidType() {

59b9b8e6ad920e29964a75d27dac48011d594fd5
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-27 23:20:22 -0800

    add

--- lesson22/task1/index.js
+++ lesson22/task1/index.js
@@ -2,39 +2,10 @@ const divElem = document.querySelector('.rect_div');
 const pElem = document.querySelector('.rect_p');
 const spanElem = document.querySelector('.rect_span');
 
-const logTarget = (text, color) => {
-
-    const eventsListElem = document.querySelector('.events-list');
-
-    eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`
-}
-
-const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
-const logGreenP = logTarget.bind(null, 'P', 'green');
-const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
-
-
-divElem.addEventListener('click', logGreenDiv);
-pElem.addEventListener('click', event => {
-    logGreenP();
-    event.stopPropagation();
-});
-spanElem.addEventListener('click', logGreenSpan);
-
-pElem.addEventListener('click', event => {
-    logGreenP();
-    console.log(event);
-});
-
-const divElem = document.querySelector('.rect_div');
-const pElem = document.querySelector('.rect_p');
-const spanElem = document.querySelector('.rect_span');
 
 const logTarget = (text, color) => {
-
-    const eventsListElem = document.querySelector('.events-list');
-
-    eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`
+    const eventListElem = document.querySelector('.events-list');
+    eventListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px">${text}</span>`
 }
 
 const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
@@ -45,12 +16,10 @@ const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
 const logGreyP = logTarget.bind(null, 'P', 'grey');
 const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
 
+divElem.addEventListener("click", logGreyDiv, true);
+pElem.addEventListener("click", logGreyP, true);
+spanElem.addEventListener("click", logGreySpan, true);
 
-divElem.addEventListener('click', logGreyDiv, true);
-divElem.addEventListener('click', logGreenDiv);
-
-pElem.addEventListener('click', logGreyP, true);
-pElem.addEventListener('click', logGreenP);
-
-spanElem.addEventListener('click', logGreySpan, true);
-spanElem.addEventListener('click', logGreenSpan);
+divElem.addEventListener("click", logGreenDiv);
+pElem.addEventListener("click", logGreenP);
+spanElem.addEventListener("click", logGreenSpan);

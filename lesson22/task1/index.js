85e4eef0e1e2b4ba3c9c8be730bc2783f8db1eb1
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-03-02 14:14:41 -0800

    add

--- lesson22/task1/index.js
+++ lesson22/task1/index.js
@@ -1,53 +1,48 @@
 const divElem = document.querySelector('.rect_div');
 const pElem = document.querySelector('.rect_p');
 const spanElem = document.querySelector('.rect_span');
-const clearBtn = document.querySelector('.clear-btn');
-const removeHandlersBtn = document.querySelector('.remove-handlers-btn');
-const attachHandlersBtn = document.querySelector('.attach-handlers-btn');
+const attachment = document.querySelector(".attach-handlers-bth");
+const remove = document.querySelector(".remove-handlers-btn");
+const clear = document.querySelector('.clear-btn');
+const placeShow = document.querySelector(".events-list");
 
 const logTarget = (text, color) => {
-    const eventsListElem = document.querySelector('.events-list');
-    eventsListElem.innerHTML += `<span style="color:${color}; margin-left:8px">${text}</span>`;
+    placeShow.innerHTML += `<span style ="color : ${color}; margin-left: 8px">${text}</span>`
 };
 
 const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
 const logGreenP = logTarget.bind(null, 'P', 'green');
-const logGreenSPAN = logTarget.bind(null, 'SPAN', 'green');
+const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
 
 const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
 const logGreyP = logTarget.bind(null, 'P', 'grey');
-const logGreySPAN = logTarget.bind(null, 'SPAN', 'grey');
+const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
 
-logGreyDiv();
-logGreyP();
-logGreySPAN();
-logGreenDiv();
-logGreenP();
-logGreenSPAN();
-
-const clearBtnFunc = () => {
-    const eventsListElem = document.querySelector('.events-list');
-    eventsListElem.innerHTML = '';
-};
-clearBtn.addEventListener('click', clearBtnFunc);
-
-const removeHandlersBtnFunc = () => {
-    divElem.removeEventListener('click', logGreyDiv, { capture: true });
-    pElem.removeEventListener('click', logGreyP, true);
-    spanElem.removeEventListener('click', logGreySPAN, true);
-
+const delet = () => {
     divElem.removeEventListener('click', logGreenDiv);
     pElem.removeEventListener('click', logGreenP);
-    spanElem.removeEventListener('click', logGreenSPAN);
+    spanElem.removeEventListener('click', logGreenSpan);
+
+    divElem.removeEventListener('click', logGreyDiv, true);
+    pElem.removeEventListener('click', logGreyP, true);
+    spanElem.removeEventListener('click', logGreySpan, true);
 };
-const attachHandlersBtnFunk = () => {
-    divElem.addEventListener('click', logGreyDiv, { capture: true });
+
+
+const add = () => {
+    divElem.addEventListener('click', logGreyDiv, true);
     pElem.addEventListener('click', logGreyP, true);
-    spanElem.addEventListener('click', logGreySPAN, true);
+    spanElem.addEventListener('click', logGreySpan, true);
 
     divElem.addEventListener('click', logGreenDiv);
     pElem.addEventListener('click', logGreenP);
-    spanElem.addEventListener('click', logGreenSPAN);
+    spanElem.addEventListener('click', logGreenSpan);
 };
-attachHandlersBtn.addEventListener('click', attachHandlersBtnFunk);
-removeHandlersBtn.addEventListener('click', removeHandlersBtnFunc);
+
+add();
+
+attachment.addEventListener('click', add);
+remove.addEventListener('click', delet);
+clear.addEventListener('click', () => {
+    placeShow.innerHTML = '';
+});

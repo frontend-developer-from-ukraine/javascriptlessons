570e8f1305cfa206d1809cb49039a742dd5f907a
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-03-02 07:25:22 -0800

    add

--- lesson22/task1/index.js
+++ lesson22/task1/index.js
@@ -19,3 +19,7 @@ const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
 divElem.addEventListener("click", logGreyDiv, true);
 pElem.addEventListener("click", logGreyP, true);
 spanElem.addEventListener("click", logGreySpan, true);
+
+divElem.addEventListener("click", logGreenDiv);
+pElem.addEventListener("click", logGreenP);
+spanElem.addEventListener("click", logGreenSpan);
